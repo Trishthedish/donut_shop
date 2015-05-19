5ed2bf332d7242b6eeecadda20b9c695e7a93e1a
Trish Griego <pmgriego@gmail.com>
2015-05-19 16:47:20 -0700

    Fixed and completed branch for model branch from previous hwk

--- donut_shop.js
+++ donut_shop.js
@@ -1,49 +1,57 @@
-var DonutShopSuccess = function(dLocation,dMin,dMax, avgSold) {
+var DonutShop = function(dLocation,dMin,dMax, avgSold) {
   this.dLocation = dLocation;
   this.dMin      = dMin;
   this.dMax      = dMax;
   this.avgSold   = avgSold;
 }
 
-DonutShopSuccess.prototype.generateRandom = function(dMin, dMax) {
+DonutShop.prototype.generateRandom = function(dMin, dMax) {
   return Math.floor(Math.random() * (this.dMax - this.dMin +1)) + this.dMin;
 }
 
-DonutShopSuccess.prototype.hourlyDonuts = function() {
+DonutShop.prototype.hourlyDonuts = function() {
   var random = this.generateRandom(this.dMin * this.dMax);
   return this.avgSold * random;
   }
+
+var allDonutShops = [];
+allDonutShops.push(new DonutShop('downtown', 8, 43, 4.5));
+allDonutShops.push(new DonutShop('caphill', 4, 37, 2.0));
+allDonutShops.push(new DonutShop('southlake', 9, 23, 6.33));
+allDonutShops.push(new DonutShop('wedgeWood', 2, 28, 1.25));
+allDonutShops.push(new DonutShop('ballard', 8, 58, 3.75));
+
+var tbl = document.getElementById('tbl');
+
+//makes this program scalable, you wont have to change it to accomodate changes in the array (in this case: shops)//
+var idx = 0;
+var len = allDonutShops.length;
+var tData;
+while(idx < len) {
+  var tRow = document.createElement('tr');
+  var tData = document.createElement('td');
+  tData.innerHTML = allDonutShops[idx].dLocation;
+  tRow.appendChild(tData);
+  console.log(allDonutShops[idx].dLocation);
+  var hours = 13;
+  var count = 0;
+  var sumTotal = 0;
+  while(count < hours + 1){
+    var tData = document.createElement('td');
+    var x = allDonutShops[idx].hourlyDonuts();
+    x = Math.floor(x);
+    tData.innerHTML = x;
+    tRow.appendChild(tData);
+    sumTotal = sumTotal + x;  //in future use +=s
+    count ++;
+  }
+console.log(sumTotal);
+sumTotal= Math.floor(sumTotal);
+tData.innerHTML = sumTotal;
+tRow.appendChild(tData);
+tbl.appendChild(tRow);
+  idx ++;
 }
-downtown = new DonutShopSuccess('downtown', 8, 43, 4.5);
-caphill  = new DonutShopSuccess('caphill', 4, 37, 2.0);
-southlake   = new DonutShopSuccess('southlake', 9, 23, 6.33);
-wedgeWood = new DonutShopSuccess('wedgeWood', 2, 28, 1.25);
-ballard   = new DonutShopSuccess('ballard', 8, 58, 3.75);
 
-calculateTablehourlyDonutes = function(dLocation){
-  var rowposition = document.getElementsByTagName(dlocation, DonutShopSuccess)
-  var totalRead = 0
-  for (i = 0; i < arrayLenth; i++) {
-    var hourRead = dlocation.calculateTablehourlyDonuts();
-    totalRead += hourRead
 
-    var newTable = document.createElement('td');
-  newTable.textcontent = hourlyRead;
-  rowPosition.appendChild(newTable);
-}
-    var newTable = document.createElement('td');
-  newTable.textcontent = totalRead;
-  rowPosition.appendChild(newTable);
-}
 
-  calculateTablehourlyDonutes(downtown);
-  calculateTablehourlyDonutes(caphill);
-  calculateTablehourlyDonutes(southlake);
-  calculateTablehourlyDonutes(WedgeWood);
-  calculateTablehourlyDonutes(ballard);
-
-// console.log(downtownD.dailyDonuts());
-// console.log(caphillD.dailyDonuts());
-// console.log(southLD.dailyDonuts());
-// console.log(wedgeWood.dailyDonuts());
-// console.log(ballard.dailyDonuts());
